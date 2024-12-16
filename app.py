e298d20a94e9fef561d99a24577b36f779536ec7
ShiblaTP <shiblashilu@gmail.com>
2024-12-16 17:36:49 +0400

    today commit

--- app.py
+++ app.py
@@ -453,7 +453,7 @@ def fetch_and_update_data():
             "UniqueFeatures" : "uniqueFeatures",
         }
 
-        # send_to_airtable_if_new(df, airtable_new, unique_field='uniqueId')
+        send_to_airtable_if_new(df, airtable_new, unique_field='uniqueId')
         send_to_airtable_if_new(
             filtered_df,
             airtable_new1,
